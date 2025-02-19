2326f7ba7a6fceedf9c3a1d494b6f8918799f7e7
Konstantin Dzyuba <konstantin.dzyuba.2003@gmail.com>
2025-02-19 18:45:03 +0300

    add retrofit dep

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,8 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.squareup.retrofit)
+    implementation(libs.squareup.converter.gson)
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
